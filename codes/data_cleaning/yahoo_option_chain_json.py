053fe1f636834ddeea9739f4244a821bbe322673
jjwalkerwvu <36414757+jjwalkerwvu@users.noreply.github.com>
2020-03-28 02:11:26 +0100

    Add files via upload

--- codes/data_cleaning/yahoo_option_chain_json.py
+++ codes/data_cleaning/yahoo_option_chain_json.py
@@ -37,6 +37,7 @@ import numpy as np
 from functools import reduce
 from datetime import datetime
 from datetime import date
+from datetime import timedelta
 from dateutil.tz import *
 import time
 ## import what we need to automatically write output to a folder
@@ -78,12 +79,20 @@ def yahoo_option_chain_json(write_path,ticker,date):
 	## the user input! Then make another query.
 	## closest date is whichever entry in expiry date has the smallest 
 	## difference to the user input date.
+	exp_array=np.array(expiry_dates)*1.0
 	## closest date in unix time. Convert to datetime later
-	#closest_date=''
-	#url_string='https://query1.finance.yahoo.com/v7/finance/options/'+ticker+'?date='+closest_date
-	## An old example:
-	#url_string='https://query1.finance.yahoo.com/v7/finance/options/'+ticker+'?date=1589500800'
+	closest_date=exp_array[((exp_array-date)**2)==np.min((exp_array-date)**2)]
+	## the actual time of day when the option expires; try 4pm EDT, although  
+	## the holder of the option has until 5pm to exercise the option, 5:30 pm 
+	## according to the nasdaq? Double check!
+	## exp_time variable should eventuall go to the top of this script?
+	#exp_time=' 16' ## 4pm?
+	#exp_time='16:00'
+	exp_date=datetime.utcfromtimestamp(closest_date)+timedelta(hours=16)
+	url_string='https://query1.finance.yahoo.com/v7/finance/options/'+ticker+'?date='+closest_date
 	
+	
+	bs_json = pd.io.json.read_json(url_string)
 	strikes=bs_json['optionChain']['result'][0][entries[2]]
 	quote=bs_json['optionChain']['result'][0][entries[4]]
 	## could also use midpoint of bid ask?
@@ -107,50 +116,6 @@ def yahoo_option_chain_json(write_path,ticker,date):
 
 
 	##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-	## A little snippet of code that I will use to get the time to expiration  
-	## in days
-
-	## the actual time of day when the option expires; try 4pm EDT, although  
-	## the holder of the option has until 5pm to exercise the option, 5:30 pm 
-	## according to the nasdaq? Double check!
-
-	## exp_time variable should eventuall go to the top of this script?
-	#exp_time=' 16' ## 4pm?
-	exp_time='16:00'
-
-	## This test date is how the dates are usually formatted on marketwatch.com.
-	#test_date='September 4, 2020'+exp_time
-	
-	## We should get the expiry date from the dataframe, calls or puts
-	#exp_string=df_calls[df_calls.columns[0]][0]
-	## remove the ticker; This does not work for spx or other index options!
-	## Maybe better to split based on the year, which is known from tnow?
-	## Anything before the tens place of the year corresponds to characters
-	## relating to the ticker, but not necessarily containing the ticker 
-	## exactly 
-	#exp_string=exp_string.split(ticker)[1]
-	## turn the exp_string into a datetime.
-	## first two digits are the expiration year; '20' is assumed for the 
-	## thousands and hundreds place for now.
-	#exp_year='20'+exp_string[:2]
-	#exp_month=exp_string[2:4]
-	#exp_day=exp_string[4:6]
-	#exp_date=pd.to_datetime(exp_year+'-'+exp_month+'-'+exp_day+'-'+exp_time)
-	
-	## We should get the expiry date from the dataframe, calls or puts
-	exp_string=df_calls[df_calls.columns[0]][0]
-	## Here is a better way, ASSUMING YAHOO FINANCE WILL ALWAYS USE THE
-	## CONTRACT NAME FORMAT: "ticker info"+"exp date"+"c or p"+"strike"
-	temp=re.findall(r'\d+', exp_string)
-	## temp should have two components; the first one is for the date and 
-	## second is for the strike.
-	exp_year='20'+temp[0][:2]
-	exp_month=temp[0][2:4]
-	exp_day=temp[0][4:6]
-	exp_date=pd.to_datetime(exp_year+'-'+exp_month+'-'+exp_day+'-'+exp_time)
-
-	#test_date = datetime.now()+exp_time
-	#date_dt = datetime.datetime.strptime(test_date, '%B %d, %Y, %H')
 
 	## get the time right now, as the code is run.
 	#tnow=pd.to_datetime('today').now()
